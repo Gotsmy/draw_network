4979ce18b0ac1a0f9ad0abfa4f8635737338d202
Gotsmy <mathias.gotsmy@univie.ac.at>
2024-11-18 10:33:32 +0100

    updated paths

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../draw_network'))
+sys.path.insert(0, os.path.abspath('../draw_network'))
 
 
 
@@ -57,7 +57,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['source/_static']
 
 def setup(app):
-    app.add_css_file("custom.css")
+    app.add_css_file("source/custom.css")
